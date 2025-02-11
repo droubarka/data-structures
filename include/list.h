c6f60bb36bd39adbaa874def20ccefe108809023
Mohamed A. Oubarka <droubarka@gmail.com>
2025-02-11 12:16:50 -0500

    Add list_map function to transform list elements
    	- Implemented `list_map()` to apply a transformation function on each list element and return a new list.
    	- Added null checks for `fn` and `del` to ensure safety.
    	- If allocation or transformation fails, the created list is cleared and memory is freed.
    	- Updated `list.h` to include the function prototype.

--- include/list.h
+++ include/list.h
@@ -17,11 +17,13 @@ size_t	list_size(t_list *head);
 
 t_list	*list_new(void *content);
 t_list	*list_last(t_list *head);
+t_list	*list_map(t_list *head, void *(*fn)(void *), void (*del)(void *));
 
 void	list_iter(t_list *head, void (*fn)(void *));
 void	list_delone(t_list *node, void (*del)(void *));
 void	list_addfront(t_list **head, t_list *node);
 void	list_addback(t_list **head, t_list *node);
 void	list_clear(t_list **head, void (*del)(void *));
+
 
 #endif

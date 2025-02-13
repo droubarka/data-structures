895a52a6604e7de7f1b1dcfa08b53980e4afb479
Mohamed A. Oubarka <droubarka@gmail.com>
2025-02-12 20:35:31 -0500

    Refactor list_duplicate to use list_map
    	- Removed redundant duplication logic.
    	- Simplified implementation by delegating to `list_map`.
    	- Ensured `dup` and `del` function pointers are utilized consistently.
    
    This change improves code reuse, reduces duplication, and enhances maintainability.

--- list/src/list_duplicate.c
+++ list/src/list_duplicate.c
@@ -4,39 +4,7 @@
 t_list	*list_duplicate(t_list *head, void *(*dup)(void *), void (*del)(void *))
 {
 	t_list	*new_list;
-	t_list	*new_node;
-	t_list	*last_node;
-	void	*content;
 
-	if (dup == NULL || del == NULL)
-		return (NULL);
-
-	new_list = NULL;
-	while (head != NULL)
-	{
-		content = dup(head->content);
-		if (content == NULL)
-		{
-			list_clear(&new_list, del);
-			break ;
-		}
-		new_node = list_new(content);
-		if (new_node == NULL)
-		{
-			del(content);
-			list_clear(&new_list, del);
-			break ;
-		}
-		if (new_list != NULL)
-		{
-			list_addafter(last_node, new_node);
-		}
-		else
-		{
-			new_list = new_node;
-		}
-		last_node = new_node;
-		head = head->next;
-	}
+	new_list = list_map(head, dup, del);
 	return (new_list);
 }

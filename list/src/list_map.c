fb2621778f35c5c335f0b5a4be0d011651159b90
Mohamed A. Oubarka <droubarka@gmail.com>
2025-02-12 20:25:27 -0500

    Refactor list_map to use list_addafter instead of list_addback
    	- Introduced `last_node` to track the last element in the new list.
    	- Replaced `list_addback` with `list_addafter` for better control over node insertion.
    	- Ensured `new_list` is initialized properly with the first node.

--- list/src/list_map.c
+++ list/src/list_map.c
@@ -5,29 +5,38 @@ t_list	*list_map(t_list *head, void *(*fn)(void *), void (*del)(void *))
 {
 	t_list	*new_list;
 	t_list	*new_node;
+	t_list	*last_node;
 	void	*content;
 
+	if (fn == NULL || del == NULL)
+		return (NULL);
+
 	new_list = NULL;
-	if (fn != NULL && del != NULL)
+	while (head)
 	{
-		while (head)
+		content = fn(head->content);
+		if (content == NULL)
 		{
-			content = fn(head->content);
-			if (content == NULL)
-			{
-				list_clear(&new_list, del);
-				break ;
-			}
-			new_node = list_new(content);
-			if (new_node == NULL)
-			{
-				del(content);
-				list_clear(&new_list, del);
-				break ;
-			}
-			list_addback(&new_list, new_node);
-			head = head->next;
+			list_clear(&new_list, del);
+			break ;
 		}
+		new_node = list_new(content);
+		if (new_node == NULL)
+		{
+			del(content);
+			list_clear(&new_list, del);
+			break ;
+		}
+		if (new_list != NULL)
+		{
+			list_addafter(last_node, new_node);
+		}
+		else
+		{
+			new_list = new_node;
+		}
+		last_node = new_node;
+		head = head->next;
 	}
 	return (new_list);
 }

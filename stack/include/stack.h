6164f42450e0a767e84fdbd0fb9b48a2b92149a8
Mohamed A. Oubarka <droubarka@gmail.com>
2025-07-16 00:25:52 +0100

    feat(stack): add initial stack implementation with stack_new, stack_push, and stack_pop

---
+++ stack/include/stack.h
@@ -0,0 +1,19 @@
+
+#ifndef STACK_H
+#define STACK_H
+
+#include <stdlib.h>
+
+typedef struct s_stack	t_stack;
+
+struct s_stack
+{
+	void	*data;
+	void	*next;
+};
+
+t_stack	*stack_new(void *data);
+t_stack	*stack_pop(t_stack **stack);
+t_stack	*stack_push(t_stack **stack, t_stack *node);
+
+#endif
